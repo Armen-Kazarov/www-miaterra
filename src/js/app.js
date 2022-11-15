39f9201a24af39289b480145edd20484ce6c63cc
Armen-Kazarov <dunkan_mc_laud@ukr.net>
2022-11-16 00:19:07 +0100

    add burger menu, scroll on click, fixed container

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import * as flsFunctions from "./modules/functions.js";
+import * as burger from "./modules/burgerMenu.js";
 
 flsFunctions.isWebp();
+burger.burgerMenu();
 
 import Swiper, { Navigation, Pagination } from 'swiper';
