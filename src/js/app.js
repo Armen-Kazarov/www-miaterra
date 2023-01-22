8dc45de29b613bbb25e01269037289c3fbabe80a
Armen-Kazarov <dunkan_mc_laud@ukr.net>
2023-01-22 22:24:02 +0100

    add slider, but will be contunie it

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,37 @@ import * as burger from "./modules/burgerMenu.js";
 flsFunctions.isWebp();
 burger.burgerMenu();
 
-import Swiper, { Navigation, Pagination } from 'swiper';
+function initSlider() {
+new Swiper('.swiper', {
+  loop: false,
+  sped: 2400,
+  navigation:{
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 1025px
+    1025: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      autoplay: false,
+    }
+  }
+});
+}
+
+initSlider();
